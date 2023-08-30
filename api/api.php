78c9d6b95b1ab9403403e63c83e8c1d836fac5ce
Jhowbrges <jhotimao@hotmail.com>
2023-08-30 12:28:04 -0300

    debug cod. api.php

--- api/api.php
+++ api/api.php
@@ -2,8 +2,8 @@
 
 // Dados da API do ChatGPT
 putenv("OPENAI_API_KEY=$_ENV[OPENAI_API_KEY]");
-$apiKey = $_ENV['OPENAI_API_KEY'];
-echo "API Key: $apiKey";
+$api_key = $_ENV['OPENAI_API_KEY'];
+echo "API Key: $api_key";
 $api_url = 'https://api.openai.com/v1/chat/completions';
 
 // Obter a mensagem do usuário do corpo da requisição
