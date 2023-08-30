faba741c18e9576cccace96cd8be529fa7e3320a
Jhowbrges <jhotimao@hotmail.com>
2023-08-30 12:14:45 -0300

    retificacao caminho de caminho api.php

--- api/api.php
+++ api/api.php
@@ -1,7 +1,7 @@
 <?php
 
 // Dados da API do ChatGPT
-$apiKey = getenv('OPENAI_API_KEY');
+$apiKey = $_ENV['OPENAI_API_KEY'];
 $api_url = 'https://api.openai.com/v1/chat/completions';
 
 // Obter a mensagem do usuário do corpo da requisição
